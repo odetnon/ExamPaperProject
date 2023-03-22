1d5ec13595b5ad678fd60303cc3bd769917a9404
Oscar Detnon <58948641+odetnon@users.noreply.github.com>
2023-03-22 19:56:28 +0000

    Install PyQt5 if the user doesn't have it installed.

--- Scripts/RunFrontend.py
+++ Scripts/RunFrontend.py
@@ -1,6 +1,12 @@
 # in this file we run the frontend (like a __main__)
 from Frontend.MainWindowHandler import MainWindowHandler
-from PyQt5.QtWidgets import QApplication
+from subprocess import call
+
+try:
+    from PyQt5.QtWidgets import QApplication
+except ImportError:
+    print("Attempting install of missing module PyQt5")
+    call("pip3 install PyQt5")
 import sys
 
 
